2ba044f0ccd6a7c6787f03af26752fb6e30fd24a
Kevin Villegas <kevin.villegas94@gmail.com>
2022-05-19 13:14:49 -0700

    added more styling and added information to about component

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,21 @@
 const About = () => {
-    return <div>hi</div>;
+    return (
+        <div className="about">
+            <h2>What is Valorant?</h2>
+            <p>
+                It's a 5v5 multiplayer first-person shooter (FPS) where one team
+                attacks and the other defends. The main game mode, Search and
+                Destroy, is very similar to CS:GO. The attacking team's goal is
+                to plant a bomb (called a spike) and have it detonate, while the
+                defending team tries to avoid that. Each player selects an agent
+                at the beginning of the game that has unique abilities and plays
+                a specific role in the game.
+            </p>
+            <a href="https://www.youtube.com/watch?v=umWJCOh_oE4&ab_channel=ProGuidesValorantTips%2CTricksandGuides">
+                Valorant explained!
+            </a>
+        </div>
+    );
 };
 
 export default About;
