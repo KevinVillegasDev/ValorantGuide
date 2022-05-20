99fa10d086c9ab92b005af867fa9afa341964a06
Kevin Villegas <kevin.villegas94@gmail.com>
2022-05-20 14:27:54 -0700

    changed deployment name

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 const Home = () => {
     return (
         <div>
-            <h1>One stop shop for all your Valorant info</h1>
+            <h1>Beginner Guide to Valorant</h1>
         </div>
     );
 };
